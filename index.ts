7bd28fef9f4704a69520c2526b1537735cf6987c
SuguruOoki <ohki_suguru@sue-san.dev>
2024-02-05 10:01:46 +0900

    modify: modeの引数を追加し、projectとuserを見れるようにした

--- index.ts
+++ index.ts
@@ -5,47 +5,87 @@ import { IssueCreateInput } from "@linear/sdk/dist/_generated_documents";
 import { ArgumentParser } from "argparse"
 
 async function main() {
-    const parser = new ArgumentParser({
-        description: 'Command line tool to create issues in bulk with Linear.',
-        add_help: true
-    })
-    parser.add_argument('-f', '--filePath', {help: "csv file path | ./test.csv", required: false, default: './test.csv'})
+  const parser = new ArgumentParser({
+    description: 'Command line tool to create issues in bulk with Linear.',
+    add_help: true
+  })
+  parser.add_argument('-m', '--mode', {help: "mode: createIssues | displayProjects | displayUsers", required: false, default: 'createIssues'})
+  parser.add_argument('-f', '--filePath', {help: "csv file path | ./test.csv", required: false, default: './test.csv'})
+  const args = parser.parse_args()
+  const mode: 'createIssues' | 'displayProjects' | 'displayUsers' = args.mode
 
-    const args = parser.parse_args()
-    const apiKey = fs.readFileSync('.api_key', 'utf8');
-    const defaultTeamId = fs.readFileSync('.default_team_id', 'utf8');
-    const defaultProjectId = fs.readFileSync('.default_project_id', 'utf8');
-    const linearClient = new LinearClient({ apiKey });
+  switch (mode) {
+    case 'createIssues':
+      createIssues(args.filePath)
+    case 'displayProjects':
+      displayProjects()
+    case 'displayUsers':
+      displayUsers()
+    default:
+      createIssues(args.filePath)
+  }
+}
 
-    if (defaultTeamId !== '') {
-      const data = fs.readFileSync(args.filePath)
-      const records = parse(data, { columns: true, });
+async function displayUsers() {
+  const apiKey = fs.readFileSync('.api_key', 'utf8');
+  const linearClient = new LinearClient({ apiKey });
+  const users =await linearClient.users();
+  if (!users) {
+    console.log('users not found');
+    return;
+  }
+  for (const user of users.nodes) {
+    console.log(user.id +': '+ user.name)
+  }
+}
+async function displayProjects() {
+  const apiKey = fs.readFileSync('.api_key', 'utf8');
+  const linearClient = new LinearClient({ apiKey });
+  const projects =await linearClient.projects();
+  if (!projects) {
+    console.log('project not found');
+    return;
+  }
+  for (const project of projects.nodes) {
+    console.log((await project.teams()).nodes[0].name + ' | '+ project.id + ': ' + project.name)
+  }
+}
 
-      for (const record of records) {
-        let insertRecord = {} as IssueCreateInput
-        insertRecord.teamId = record.teamId;
-        insertRecord.title = record.title
-        insertRecord.description = record.description
-        insertRecord.projectId = record.projectId
-        insertRecord.assigneeId = record.assigneeId
+function createIssues(filePath: string) {
+  const apiKey = fs.readFileSync('.api_key', 'utf8');
+  const defaultTeamId = fs.readFileSync('.default_team_id', 'utf8');
+  const defaultProjectId = fs.readFileSync('.default_project_id', 'utf8');
+  const linearClient = new LinearClient({ apiKey });
 
-        if (record.assigneeId === '') {
-          delete insertRecord.assigneeId;
-        }
+  if (defaultTeamId !== '') {
+    const data = fs.readFileSync(filePath)
+    const records = parse(data, { columns: true, });
 
-        if (record.teamId === '') {
-          insertRecord.teamId = defaultTeamId;
-        }
+    for (const record of records) {
+      let insertRecord = {} as IssueCreateInput
+      insertRecord.teamId = record.teamId;
+      insertRecord.title = record.title
+      insertRecord.description = record.description
+      insertRecord.projectId = record.projectId
+      insertRecord.assigneeId = record.assigneeId
 
-        if (record.projectId === '') {
-          insertRecord.projectId = defaultProjectId;
-        }
-
-        linearClient.createIssue(
-          insertRecord
-        );
+      if (record.assigneeId === '') {
+        delete insertRecord.assigneeId;
       }
+
+      if (record.teamId === '') {
+        insertRecord.teamId = defaultTeamId;
+      }
+
+      if (record.projectId === '') {
+        insertRecord.projectId = defaultProjectId;
+      }
+
+      linearClient.createIssue(
+        insertRecord
+      );
     }
+  }
 }
 
 main();
